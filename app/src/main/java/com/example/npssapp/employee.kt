317b4c0481e61bbc4c4739df5787fc0450d72c5e
DanielCoder1337 <49248906+danielcoder1337@users.noreply.github.com>
2020-09-17 17:41:04 +0200

    Fixed delete and create function and added getEmployee function

--- app/src/main/java/com/example/npssapp/employee.kt
+++ app/src/main/java/com/example/npssapp/employee.kt
@@ -1,5 +1,6 @@
 package com.example.npssapp
 
+import android.util.Log
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
@@ -8,10 +9,7 @@ import com.google.firebase.ktx.Firebase
 val database = Firebase.database.reference
 
 @IgnoreExtraProperties
-data class Employee(
-    var uId: Int,
-    var title: String
-)
+data class Employee(val uid: Int = 0, val title: String = "")
 
 fun createNewEmployee(uId: Int,title: String): Array<String> {
     val employee = Employee(uId, title)
@@ -21,15 +19,35 @@ fun createNewEmployee(uId: Int,title: String): Array<String> {
         outcome += "No title with that name."
         return outcome
     }
-    database.child("users").setValue(employee)
+    database.child("employees").child(employee.uid.toString()).setValue(employee)
         .addOnFailureListener {
             outcome += "Could not write to database!"
         }
     return outcome
 }
 
+fun getEmployee(uId: Int, callback: (result: Employee?) -> Unit){
+    val employees = database.child("employees")
+    employees.addListenerForSingleValueEvent(object: ValueEventListener{
+        override fun onCancelled(error: DatabaseError) {
+        }
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val employeeDetails = snapshot.children
+            employeeDetails.forEach {
+                val childEmployee = it.getValue(Employee::class.java)
+                if (childEmployee != null) {
+                    if (childEmployee.uid == uId){
+                        callback.invoke(childEmployee)
+                    }
+                }
+                else callback(null)
+            }
+        }
+    })
+}
+
 fun deleteEmployee(uId:Int) : Boolean{
-    val employees = database.child("users")
+    val employees = database.child("employees")
     var outcome : Boolean = false
     employees.addListenerForSingleValueEvent(object: ValueEventListener{
         override fun onCancelled(error: DatabaseError) {
@@ -37,9 +55,9 @@ fun deleteEmployee(uId:Int) : Boolean{
         override fun onDataChange(snapshot: DataSnapshot) {
             val employeeDetails = snapshot.children
             employeeDetails.forEach {
-                val employee = it.getValue<Employee>()
+                val employee = it.getValue(Employee::class.java)
                 if (employee != null) {
-                    if (employee.uId == uId){
+                    if (employee.uid == uId){
                         it.ref.removeValue()
                             .addOnSuccessListener {
                                 outcome = true
