2764d02c5eeb4d8e0ed95c3be4512bd48e3cd623
kepan13 <meos1713@student.ju.se>
2020-09-18 12:30:58 +0200

    Bluetooth now working

--- hardware/cardReader/cardReader.ino
+++ hardware/cardReader/cardReader.ino
@@ -1,37 +1,52 @@
-#include <SoftwareSerial.h>
+char c;
+ 
+#include <SoftwareSerial.h> 
 #include <SPI.h>
 #include <MFRC522.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+SoftwareSerial BTserial(2, 3); // RX, TX
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
-  Serial.println("Approximate your card to the reader...");
+  Serial.println("Enter AT commands or approximate your card reader...:");
   Serial.println();
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  // Look for new cards
-  if ( !mfrc522.PICC_IsNewCardPresent()) {
-    return ;
+  BTserial.begin(9600);
+}
+ 
+void loop() 
+{
+
+  if (BTserial.available())
+  {
+    Serial.write(BTserial.read());
   }
-  // Select one of the cards
-  if (!mfrc522.PICC_ReadCardSerial()) {
+  
+  if (Serial.available())
+  {
+    BTserial.write(Serial.read());
+  }
+
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
     return;
   }
-
-  // Show UID on serial monitor
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+    // Show UID on serial monitor
   Serial.print("UID tag :");
   String content = "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
@@ -40,13 +55,14 @@ void loop() {
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "D9 A7 DD 56" || content.substring(1) == "99 56 AE 47"){
+  if (content.substring(1) == "D9 A7 DD 56" || content.substring(1) == "99 56 AE 47")
+  {
     Serial.println("Authorized access =)");
     Serial.println();
     delay(3000);
   }
-  else {
+  else 
+  {
     Serial.println(" Access denied");
   }
-  
 }
