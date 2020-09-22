6f8def9699a756a1f74ea0f21393299c212d4155
Joel Hermansson <hejo1797@student.ju.se>
2020-09-22 16:25:52 +0200

    added the lcd-display (hello world

--- hardware/main/main.ino
+++ hardware/main/main.ino
@@ -1,12 +1,15 @@
 #include <SoftwareSerial.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <LiquidCrystal.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 SoftwareSerial hc06(2,3);
+
+LiquidCrystal lcd(A0, A1, 7, 6, 5, 4);
 
 bool isLoggedIn = false;
 
@@ -19,6 +22,11 @@ void setup(){
   Serial.println();
   //Initialize Bluetooth Serial Port
   hc06.begin(9600);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
 
 void loop(){
@@ -48,5 +56,14 @@ void loop(){
   Serial.println();
   // Sends string to bluetooth unit
   hc06.print(uid);
+
+
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+
+  
   delay(3000);
 }
