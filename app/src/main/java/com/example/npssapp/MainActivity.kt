d1b5c07f183b13a7eb957ea5bebc5f154976d8fd
DanielCoder1337 <49248906+danielcoder1337@users.noreply.github.com>
2020-09-24 10:26:18 +0200

    Added MainActivity.

---
+++ app/src/main/java/com/example/npssapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.npssapp
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        lateinit var mBluetoothAdapter: BluetoothAdapter
+        const val REQUEST_ENABLE_BLUETOOTH = 1
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(!mBluetoothAdapter.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+        else if (mBluetoothAdapter == null) {
+            Log.d("Crash", "Bluetooth not supported on this device!")
+            return
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (mBluetoothAdapter.isEnabled) {
+                    Toast.makeText(applicationContext, "Bluetooth has been enabled", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(applicationContext, "Bluetooth has been disabled", Toast.LENGTH_SHORT).show()
+
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(applicationContext, "Bluetooth enabling has been canceled", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
