d01b3652e61613aa15d96e293298f70635ae9750
LudvigAng <anlu18qp@student.ju.se>
2020-09-24 02:32:26 +0200

    Added notification icon, sound, vibration and phone light-up if locked.

--- app/src/main/java/com/example/npssapp/MainActivity.kt
+++ app/src/main/java/com/example/npssapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.npssapp
 
 import android.annotation.SuppressLint
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val notificationListener = RadiationNotifications(123, this)
+        val notificationHandler = WarningNotificationHandler(123, this)
 
         createNotificationChannel()
-        //TODO("Pass user's id which is in database")
-        notificationListener.listenForClockInEntry(123)
+        //TODO("Pass user's id instead of static 123")
+        notificationHandler.listenForClockInEntry(123)
     }
 
     private fun createNotificationChannel() {
@@ -28,11 +32,19 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(1.toString(), name, importance).apply {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val soundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+            val attributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+
+            val channel = NotificationChannel(5.toString(), name, importance).apply {
                 description = descriptionText
+                lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+                setSound(soundUri, attributes)
+                enableVibration(true)
             }
-            // Register the channel with the system
+
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
--- app/src/main/java/com/example/npssapp/WarningNotificationHandler.kt
+++ app/src/main/java/com/example/npssapp/WarningNotificationHandler.kt
@@ -1,9 +1,12 @@
 package com.example.npssapp
 
-import android.app.IntentService
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.PowerManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -13,20 +16,20 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.io.Serializable
-import java.security.AccessController.getContext
 import java.util.*
 import kotlin.concurrent.schedule
 
 
-data class RadiationNotifications(val uid: Int, val context: Context) : Serializable {
+data class WarningNotificationHandler(val uid: Int, val context: Context) : Serializable {
 
     fun listenForClockInEntry(uId: Int) {
         val databaseOnlineRef = Firebase.database.reference.child("online")
 
-        databaseOnlineRef.addChildEventListener(object: ChildEventListener{
+        databaseOnlineRef.addChildEventListener(object : ChildEventListener {
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
+
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
                 TODO("Not yet implemented")
             }
@@ -34,39 +37,39 @@ data class RadiationNotifications(val uid: Int, val context: Context) : Serializ
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
                 TODO("Not yet implemented")
             }
-
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
 
-                if(snapshot.child("uid").value.toString() == uId.toString()) {
+                if (snapshot.child("uid").value.toString() == uId.toString()) {
                     val timer = Timer("schedule", true)
 
-                    //TODO("Delay until radiation limit is almost reached")
-                    timer.schedule(0) {
+                    //TODO("Delay until radiation limit is almost reached (in milliseconds)")
+                    timer.schedule(3000) {
                         sendRadiationNotification()
+                        wakePhoneScreen()
                     }
                 }
             }
-
             override fun onChildRemoved(snapshot: DataSnapshot) {
-                TODO("Not yet implemented")
+                Log.i(
+                    "Child removed",
+                    "'Online' child node removed with uid: " + snapshot.child("uid").value.toString()
+                )
             }
-
-
         })
     }
 
     fun sendRadiationNotification() {
         //intent which determines where user ends up when pressing notification
-        val intent = Intent(context, MainActivity::class.java).apply {
+        var intent = Intent(context, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
-        var builder = NotificationCompat.Builder(context, 1.toString())
-            .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+        var builder = NotificationCompat.Builder(context, 5.toString())
+            .setSmallIcon(R.drawable.ic_stat_warning)
             .setContentTitle("Radiation limit almost reached.")
-            .setContentText("REMINDER: You are about to reach the radiation limit.")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentText("WARNING: You are about to reach the radiation limit.")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
 
@@ -74,6 +77,23 @@ data class RadiationNotifications(val uid: Int, val context: Context) : Serializ
             // notificationId is a unique int for each notification that you must define
             notify(123, builder.build())
         }
+    }
 
+    fun wakePhoneScreen() {
+        val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+        val screenOn =
+            if (Build.VERSION.SDK_INT >= 20) {
+                pm.isInteractive
+            } else {
+                pm.isScreenOn
+            }
+
+            if (!screenOn) {
+                val wl = pm.newWakeLock(
+                    PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                    "app:radiationWarning"
+                )
+                wl.acquire(5000)
+            }
     }
 }
