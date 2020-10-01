384cfe31336866231817587089df2bbc938ef685
kepan13 <meos1713@student.ju.se>
2020-10-01 15:25:25 +0200

    added feature hazmat suit, and timer to lcd. Also added a function to receive strings over BT

--- hardware/main/main.ino
+++ hardware/main/main.ino
@@ -6,25 +6,84 @@
 #define SS_PIN 10
 #define RST_PIN 9
 #define MAX_TIME_SECONDS 10
+#define RX_PIN 2
+#define TX_PIN 3
+#define POT_PIN A5
+#define BUTTON_PIN 8
+
+#define BAUD_RATE 9600
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-SoftwareSerial hc06(2, 3);
+SoftwareSerial hc06(RX_PIN, TX_PIN);
 
 LiquidCrystal lcd(A0, A1, 7, 6, 5, 4);
 
 bool isLoggedIn = false;
 unsigned long timer = 0;
+int prevVal = 0;
+int buttonVal = 0;
+String msg="";
+String intToString="";
+bool hazmatSuitEquipped = false;
 
+void radiationSimulator() {
+  int readVal = analogRead(POT_PIN);
+  readVal /= 10;
+ 
+  if ( abs(readVal - prevVal) > 5 ) {
+    // send msg
+    prevVal = readVal;
+    msg = "r:";
+    intToString = String(prevVal);
+    String sendRadVal =  msg + intToString;
+    hc06.print(sendRadVal);
+    Serial.print(sendRadVal);
+    Serial.println();
+  }
+  delay(100);
+}
+
+void hazmatSuitSimulator(){
+  buttonVal = digitalRead(BUTTON_PIN);
+  if (buttonVal == HIGH) {
+    // Button pressed
+    if (hazmatSuitEquipped) {
+      hazmatSuitEquipped = false;
+      hc06.print("s:false");
+      Serial.print("s:false");
+      Serial.println();
+    } 
+    else {
+      hazmatSuitEquipped = true;
+      hc06.print("s:true");
+      Serial.print("s:true");
+      Serial.println();
+    }
+  }
+  delay(100);
+}
+
+void displaySafetyStatus(){
+  
+}
+
+void startTimer(){
+  
+}
+
+void stopTimer(){
+  
+}
 
 void setup() {
   //Initialize Serial Monitor
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
   SPI.begin();
   mfrc522.PCD_Init();
   Serial.println("Scan card/pin");
   Serial.println();
   //Initialize Bluetooth Serial Port
-  hc06.begin(9600);
+  hc06.begin(BAUD_RATE);
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -33,13 +92,17 @@ void setup() {
 }
 
 void loop() {
-
+  
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   // lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   // lcd.print(millis() / 1000);
 
+  // Send radiation level
+  radiationSimulator();
+  hazmatSuitSimulator();
+  
   // display timer of when someone is checked in
   if (timer != 0) {
     lcd.setCursor(0, 1);
@@ -50,14 +113,30 @@ void loop() {
     lcd.print(" GET OUT!");
   }
 
-
+  
   //Write data from HC06 to Serial Monitor
   if (hc06.available()) {
-    // Serial.write(hc06.read());
-    int receivedMsg = hc06.read();
-    Serial.print(receivedMsg);
-    Serial.println();
-    switch(receivedMsg) {
+    String rxMsg;
+    while(hc06.available()){
+      delay(1);
+      if(hc06.available() > 0){
+        char c = hc06.read();
+        rxMsg += c;
+      }
+    }
+  
+  char command = rxMsg[0];
+  
+  
+  String timer = rxMsg.substring(2);
+  Serial.print(timer);
+  Serial.println();
+  // special case
+  if (timer[timer.length() - 1] == 'n'){
+    command = 'n';
+  }
+  
+    switch(command) {  
       case 'r': 
         // System wide warning
         lcd.clear();
@@ -78,7 +157,11 @@ void loop() {
       case 'n': 
         // Interval warning
         lcd.clear();
-        lcd.print("Reminder warning");
+        lcd.print("Interval warning");
+        break;
+      case 't':
+        lcd.clear();
+        lcd.print(timer);
         break;
       default:
         lcd.clear();
@@ -86,12 +169,17 @@ void loop() {
         break;
     }
   }
+  
+  if (hc06.available()){
+    Serial.write(hc06.read());
+  }
 
+  
   //Write from Serial Monitor to HC06
   if (Serial.available()) {
     hc06.write(Serial.read());
   }
-
+  
   if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
@@ -99,10 +187,11 @@ void loop() {
     return;
   }
 
-  String uid = "";
+  String uid = "u:";
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     uid += String(mfrc522.uid.uidByte[i], HEX);
   }
+  
   /*if (uid == "d9a7dd56") {
     if (isLoggedIn) {
       // Reseting timer so it wont be printed
